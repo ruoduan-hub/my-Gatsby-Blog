32239ada741d03d459593cb53b95683edcc47f13
ruoduan_mac <chad97@126.com>
2020-01-02 17:27:01 +0800

    frist

---
+++ README.md
@@ -0,0 +1,10 @@
+## my gatsby 
+
+🚀 基于 Gatsby 开发自己的 blog 
+
+
+### 插件 🔥
+- add gatsby-remark-vscode 代码高亮插件
+- add antd
+
+
---
+++ gatsby-config.js
@@ -0,0 +1,103 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Starter Blog`,
+    author: `Kyle Mathews`,
+    description: `A starter blog demonstrating what Gatsby can do.`,
+    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    social: {
+      twitter: `kylemathews`,
+    },
+  },
+  plugins: [
+    
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          // 代码高亮度插件 注释：extensionDataDirectory 使用默认路径
+          {
+            resolve: `gatsby-remark-vscode`,
+            // All options are optional. Defaults shown here.
+            options: {
+              colorTheme: 'Dark+ (default dark)', // Read on for list of included themes. Also accepts object and function forms.
+              wrapperClassName: '',   // Additional class put on 'pre' tag. Also accepts function to set the class dynamically.
+              injectStyles: true,     // Injects (minimal) additional CSS for layout and scrolling
+              extensions: [],         // Extensions to download from the marketplace to provide more languages and themes
+              // extensionDataDirectory: // Absolute path to the directory where extensions will be downloaded. Defaults to inside node_modules.
+              //   path.resolve('/node_modules'),
+              languageAliases: {},    // Map of custom/unknown language codes to standard/known language codes
+              replaceColor: x => x,   // Function allowing replacement of a theme color with another. Useful for replacing hex colors with CSS variables.
+              getLineClassName: ({    // Function allowing dynamic setting of additional class names on individual lines
+                content,              //   - the string content of the line
+                index,                //   - the zero-based index of the line within the code fence
+                language,             //   - the language specified for the code fence
+                codeFenceOptions      //   - any options set on the code fence alongside the language (more on this later)
+              }) => '',
+              logLevel: 'error'       // Set to 'warn' to debug if something looks wrong
+            }
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        //trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gatsby Starter Blog`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `content/assets/gatsby-icon.png`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
