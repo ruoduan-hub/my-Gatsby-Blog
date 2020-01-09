90bc1e75d2438846f682108f6603c9a1eb306732
ruoduan_mac <chad97@126.com>
2020-01-09 18:25:50 +0800

    submit comfig

--- gatsby-config.js
+++ gatsby-config.js
@@ -97,13 +97,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Ruoduan'Blog`,
+        short_name: `Ruoduan Blog .`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/mylogo.jpg`,
       },
     },
     `gatsby-plugin-react-helmet`,
