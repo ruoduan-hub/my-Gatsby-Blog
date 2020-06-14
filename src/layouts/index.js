178fd594335cabe4cf71a61f91d8a5afd06492b3
ruoduan_mac <chad97@126.com>
2020-06-14 19:43:18 +0800

    Layouts 插件实现 路由动画

---
+++ src/layouts/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { TransitionProvider, TransitionViews } from "gatsby-plugin-transitions";
+
+
+const Layout = ({ location, children }) => {
+  return (
+    <TransitionProvider
+      location={location}
+      mode="immediate"
+      // 进入
+      enter={{
+        opacity: 1,
+        transform: "translate3d(50vh,0vh,0) scale3d(1, 1, 1) rotate(0deg)",
+        config: {
+            duration: 700
+          }
+      }}
+      // 正常
+      usual={{
+        opacity: 1,
+        transform: "translate3d(0vh,0vh,0) scale3d(1, 1, 1) rotate(0deg)"
+      }}
+      // 切出
+      leave={{
+        opacity: 0,
+        transform: "translate3d(0vh,0vh,0vh) scale3d(0.5, 0.5, 0.5) rotate(0deg)",
+        config: {
+            duration: 1000
+          }
+      }}
+    >
+      <TransitionViews>
+        {children}
+      </TransitionViews>
+    </TransitionProvider>
+  );
+};
+
+export default Layout;
