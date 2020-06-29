e7aa7085bdef79f5e435092d8674aa8f66451b70
ruoduan_mac <chad97@126.com>
2020-06-29 13:50:44 +0800

    portals 解决 fixed 下 transfrom 问题

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,11 +1,12 @@
 import React from "react"
+import ReactDOM from 'react-dom';
 import { Link, graphql } from "gatsby"
 import {
-  DoubleLeftOutlined,
-  DoubleRightOutlined,
-  EyeInvisibleOutlined,
-  EyeOutlined,
-  ProfileFilled,
+    DoubleLeftOutlined,
+    DoubleRightOutlined,
+    EyeInvisibleOutlined,
+    EyeOutlined,
+    ProfileFilled,
 } from "@ant-design/icons"
 import { BackTop } from "antd"
 
@@ -19,146 +20,171 @@ import { autoBaiduSubmit } from "../utils/utils"
 import WithDrawer from "../components/drawer"
 import Toc from "../components/toc"
 
+// portals 插槽 插到到DOM元素
+const PortalsRoot = typeof document!== 'undefined' ? document.getElementById('___gatsby'): null
+
 class BlogPostTemplate extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      eyeModel: {
-        background: null,
-      },
+    constructor() {
+        super()
+        this.state = {
+            eyeModel: {
+                background: null,
+            },
+        }
+        // 创建一个dom元素容器
+        this.el = typeof document !== `undefined` ? document.createElement('div') : null
     }
-  }
-  componentDidMount() {
-    autoBaiduSubmit()
-  }
-  render() {
-    const post = this.props.data.markdownRemark
-    const siteTitle = this.props.data.site.siteMetadata.title
-    const { previous, next } = this.props.pageContext
-
-    // 护眼模式 func
-    const changeEye = () => {
-      if (!this.state.eyeModel.background) {
-        this.setState({
-          eyeModel: { background: "#E3EDCD" },
-        })
-      } else {
-        this.setState({
-          eyeModel: { background: false },
-        })
-      }
+    componentDidMount() {
+        autoBaiduSubmit()
+        // 挂载到dom元素上
+        PortalsRoot.appendChild(this.el)
+    }
+    componentWillUnmount() {
+        // 清除元素
+        PortalsRoot.removeChild(this.el);
     }
 
-    return (
-      <>
-        <WithDrawer
-          data={
-            <Toc tocHtml={this.props.data.markdownRemark.tableOfContents} />
-          }
-          button={<ProfileFilled style={{ fontSize: "2rem" }} />}
-        />
+    render() {
+        const post = this.props.data.markdownRemark
+        const siteTitle = this.props.data.site.siteMetadata.title
+        const { previous, next } = this.props.pageContext
 
-        <div style={this.state.eyeModel} id="main">
-          <BackTop visibilityHeight={800} />
-          <div
-            id="eyeModel"
-            style={{
-              position: "fixed",
-              right: "3rem",
-              bottom: "10rem",
-              zIndex: 99,
-            }}
-          >
-            {this.state.eyeModel.background ? (
-              <EyeInvisibleOutlined
-                onClick={changeEye}
-                style={{ fontSize: "3rem" }}
-              />
-            ) : (
-              <EyeOutlined onClick={changeEye} style={{ fontSize: "3rem" }} />
-            )}
-          </div>
-          <Layout location={this.props.location} title={siteTitle}>
-            <SEO
-              title={post.frontmatter.title}
-              tags={post.frontmatter.title || post.frontmatter.title}
-              description={post.frontmatter.description || post.excerpt}
-            />
-            <article>
-              <MyNav small={true} />
-              <header>
-                <h1
-                  style={{
-                    marginTop: rhythm(1),
-                    marginBottom: 0,
-                  }}
-                >
-                  {post.frontmatter.title}
-                </h1>
-                <p
-                  style={{
-                    ...scale(1 / 5),
-                    display: `block`,
-                    marginBottom: rhythm(1),
-                  }}
-                >
-                  {post.frontmatter.date}
-                </p>
-              </header>
-              <section dangerouslySetInnerHTML={{ __html: post.html }} />
-              <hr
-                style={{
-                  marginBottom: rhythm(1),
-                }}
-              />
-              <footer>
-                <Bio />
-              </footer>
-            </article>
+        // 护眼模式 func
+        const changeEye = () => {
+            if (!this.state.eyeModel.background) {
+                this.setState({
+                    eyeModel: { background: "#E3EDCD" },
+                })
+            } else {
+                this.setState({
+                    eyeModel: { background: false },
+                })
+            }
+        }
 
-            <nav>
-              <ul
-                style={{
-                  display: `flex`,
-                  flexWrap: `wrap`,
-                  justifyContent: `space-between`,
-                  listStyle: `none`,
-                  padding: 0,
-                  margin: "2rem 0",
-                }}
-              >
-                <li>
-                  {next && (
-                    <Link className="nextText" to={next.fields.slug} rel="next">
-                      <DoubleLeftOutlined />
-                      {next.frontmatter.title}
-                    </Link>
-                  )}
-                </li>
+        return (
+            <>
+                <WithDrawer
+                    data={
+                        <Toc tocHtml={this.props.data.markdownRemark.tableOfContents} />
+                    }
+                    button={<ProfileFilled style={{ fontSize: "2rem" }} />}
+                />
 
-                <li>
-                  {previous && (
-                    <Link
-                      className="nextText"
-                      to={previous.fields.slug}
-                      rel="prev"
-                    >
-                      {previous.frontmatter.title} <DoubleRightOutlined />
-                    </Link>
-                  )}
-                </li>
-              </ul>
-            </nav>
-          </Layout>
+                {/* portals 挂载到外层 */}
+                <div style={this.state.eyeModel} id="main">
 
-          <Comment
-            gitalkConfig={this.props.data.site.siteMetadata.gitalkConfig}
-            path={this.props.path}
-          />
-        </div>
-      </>
-    )
-  }
+                    {
+                        ReactDOM.createPortal(
+                            <div
+                                id="eyeModel"
+                                style={{
+                                    position: "fixed",
+                                    right: "3rem",
+                                    top: '1rem',
+                                    zIndex: 99,
+                                }}
+                            >
+                                {this.state.eyeModel.background ? (
+                                    <div>
+                                        <EyeInvisibleOutlined
+                                            onClick={changeEye}
+                                            style={{ fontSize: "3rem" }}
+                                        />
+                                        <BackTop visibilityHeight={800} />
+                                    </div>
+                                ) : (
+
+                                        <div>
+                                            <EyeOutlined onClick={changeEye} style={{ fontSize: "3rem" }} />
+                                            <BackTop visibilityHeight={800} />
+                                        </div>
+                                    )}
+                            </div>,
+                            this.el
+                        )
+                    }
+                    <Layout location={this.props.location} title={siteTitle}>
+                        <SEO
+                            title={post.frontmatter.title}
+                            tags={post.frontmatter.title || post.frontmatter.title}
+                            description={post.frontmatter.description || post.excerpt}
+                        />
+                        <article>
+                            <MyNav small={true} />
+                            <header>
+                                <h1
+                                    style={{
+                                        marginTop: rhythm(1),
+                                        marginBottom: 0,
+                                    }}
+                                >
+                                    {post.frontmatter.title}
+                                </h1>
+                                <p
+                                    style={{
+                                        ...scale(1 / 5),
+                                        display: `block`,
+                                        marginBottom: rhythm(1),
+                                    }}
+                                >
+                                    {post.frontmatter.date}
+                                </p>
+                            </header>
+                            <section dangerouslySetInnerHTML={{ __html: post.html }} />
+                            <hr
+                                style={{
+                                    marginBottom: rhythm(1),
+                                }}
+                            />
+                            <footer>
+                                <Bio />
+                            </footer>
+                        </article>
+
+                        <nav>
+                            <ul
+                                style={{
+                                    display: `flex`,
+                                    flexWrap: `wrap`,
+                                    justifyContent: `space-between`,
+                                    listStyle: `none`,
+                                    padding: 0,
+                                    margin: "2rem 0",
+                                }}
+                            >
+                                <li>
+                                    {next && (
+                                        <Link className="nextText" to={next.fields.slug} rel="next">
+                                            <DoubleLeftOutlined />
+                                            {next.frontmatter.title}
+                                        </Link>
+                                    )}
+                                </li>
+
+                                <li>
+                                    {previous && (
+                                        <Link
+                                            className="nextText"
+                                            to={previous.fields.slug}
+                                            rel="prev"
+                                        >
+                                            {previous.frontmatter.title} <DoubleRightOutlined />
+                                        </Link>
+                                    )}
+                                </li>
+                            </ul>
+                        </nav>
+                    </Layout>
+
+                    <Comment
+                        gitalkConfig={this.props.data.site.siteMetadata.gitalkConfig}
+                        path={this.props.path}
+                    />
+                </div>
+            </>
+        )
+    }
 }
 
 export default BlogPostTemplate
