71287afcdd4d355f522db0210b0a93048c0e952c
zhurd <zhuruoduan@qq.com>
2020-10-16 17:34:25 +0800

    11

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,25 +77,11 @@ module.exports = {
         plugins: [
           // 代码高亮度插件 注释：extensionDataDirectory 使用默认路径
           {
-            resolve: `gatsby-remark-vscode`,
-            // All options are optional. Defaults shown here.
+            resolve: `gatsby-remark-highlight-code`,
             options: {
-              colorTheme: "Dark+ (default dark)", // Read on for list of included themes. Also accepts object and function forms.
-              wrapperClassName: "", // Additional class put on 'pre' tag. Also accepts function to set the class dynamically.
-              injectStyles: true, // Injects (minimal) additional CSS for layout and scrolling
-              extensions: [], // Extensions to download from the marketplace to provide more languages and themes
-              // extensionDataDirectory: // Absolute path to the directory where extensions will be downloaded. Defaults to inside node_modules.
-              //   path.resolve('/node_modules'),
-              languageAliases: {}, // Map of custom/unknown language codes to standard/known language codes
-              replaceColor: x => x, // Function allowing replacement of a theme color with another. Useful for replacing hex colors with CSS variables.
-              getLineClassName: ({
-                // Function allowing dynamic setting of additional class names on individual lines
-                content, //   - the string content of the line
-                index, //   - the zero-based index of the line within the code fence
-                language, //   - the language specified for the code fence
-                codeFenceOptions, //   - any options set on the code fence alongside the language (more on this later)
-              }) => "",
-              logLevel: "error", // Set to 'warn' to debug if something looks wrong
+              terminal: 'carbon',
+              theme: 'blackboard'
+
             },
           },
           {
