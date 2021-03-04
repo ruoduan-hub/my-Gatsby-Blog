b139673f536c1a96cf0b73aee903851b66732014
ruoduan <chad97@126.com>
2021-03-04 20:59:34 +0800

    feat: about mkFormat

--- src/components/mdx-layout/pageLayout.js
+++ src/components/mdx-layout/pageLayout.js
@@ -1,33 +1,121 @@
 import React from "react"
 import { MDXProvider } from "@mdx-js/react"
-// import { MDXRenderer } from "gatsby-plugin-mdx"
+import { StaticQuery, graphql } from 'gatsby'
+import { MDXRenderer } from "gatsby-plugin-mdx"
 import Header from '../header'
 import { Link } from "gatsby"
 import ThemeContext from '../../context/ThemeContext'
 import Comment from '../comment'
+import { GithubOutlined, WeiboOutlined, ZhihuOutlined, MailOutlined } from '@ant-design/icons'
+import { Divider } from 'antd'
+import { openPage } from '../../utils/utils'
 
 import S from './page-layout.module.scss'
 
+
 const shortcodes = { Link }
 
+
 const PageLayout = ({ children, path }) => {
+  // console.log(props)
 
   return (
-    <ThemeContext.Consumer>
-      {(theme) => (
-        <div>
-          <Header theme={theme} isHome={false} title={'About'} imgSrc='https://tva1.sinaimg.cn/large/008eGmZEly1go7wupjosrj30zk0qo43e.jpg' />
 
-          <main className={S.main}>
-            <MDXProvider components={shortcodes}>{children}</MDXProvider>
+    <StaticQuery
+      query={graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            author
+            description
+            gitalkConfig {
+              clientID
+              clientSecret
+            }
+            social {
+              github
+              zhihu
+              weibo
+              email
+              aboutLike
+              skill
+            }
+          }
+        }
+        
+        
+      }
+    `}
 
-            <Comment
-              path={path}
-            />
-          </main>
-        </div>
-      )}
-    </ThemeContext.Consumer>
+      render={data => {
+
+        const {
+          github,
+          zhihu,
+          weibo,
+          email,
+          aboutLike,
+          skill,
+        } = data.site.siteMetadata.social
+
+        return (
+          <ThemeContext.Consumer>
+            {(theme) => (
+              <div>
+                <Header theme={theme} isHome={false} title={'About'} imgSrc='https://tva1.sinaimg.cn/large/008eGmZEly1go7wupjosrj30zk0qo43e.jpg' />
+
+                <div className={theme.dark ? '' : S.isMainWhPost}>
+                  <main className={S.main}>
+
+
+                    <div className={S.mid}>
+                      <div className={S.info}>
+                        <MailOutlined
+                          onClick={() => openPage(`mailto:${email}`)}
+                          style={{ fontSize: '2rem' }}
+                          key="setting"
+                        />
+                        <GithubOutlined
+                          onClick={() => openPage(github)}
+                          style={{ fontSize: '2rem' }}
+                          key="setting"
+                        />
+                        <ZhihuOutlined
+                          onClick={() => openPage(zhihu)}
+                          style={{ fontSize: '2rem' }}
+                          key="edit"
+                        />
+                        <WeiboOutlined
+                          onClick={() => openPage(weibo)}
+                          style={{ fontSize: '2rem' }}
+                          key="ellipsis"
+                        />
+                      </div>
+
+                      <div >
+                        <MDXProvider components={shortcodes}>{children}</MDXProvider>
+                      </div>
+                      
+                      <Divider>留言</Divider>
+
+                      <Comment
+                        path={path}
+                      />
+                    </div>
+
+
+                  </main>
+                </div>
+
+              </div>
+            )}
+          </ThemeContext.Consumer>
+        )
+      }}
+    />
+
+
 
   )
 }
