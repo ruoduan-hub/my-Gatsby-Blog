5b823cc2971467bca41b1da2a4302126c1c36983
ruoduan <chad97@126.com>
2021-08-03 17:50:09 +0800

    fix: useHotkeys

--- src/components/StateSearch.js
+++ src/components/StateSearch.js
@@ -2,10 +2,13 @@ import React, { useState, useImperativeHandle, forwardRef } from 'react'
 
 import Modal from './Modal'
 import Search from './Search'
-import Hotkeys from 'react-hot-keys'
+
+import { useHotkeys } from 'react-hotkeys-hook'
 
 const StateSearch = (props, ref) => {
   const [visible, setVisible] = useState(false)
+
+  useHotkeys('shift+k,command+k', () => setVisible(true));
 
   useImperativeHandle(ref, () => ({
     // handleVisible 暴露给父组件
@@ -13,11 +16,9 @@ const StateSearch = (props, ref) => {
   }))
 
   return (
-    <Hotkeys keyName="shift+k,command+k" onKeyDown={() => setVisible(true)}>
       <Modal setVisible={setVisible} visible={visible}>
         <Search />
       </Modal>
-    </Hotkeys>
   )
 }
 
