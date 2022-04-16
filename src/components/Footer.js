875ef4a440ef71b4b803674ec85af565536d4b37
ruoduan-hub <chad97@126.com>
2022-04-16 15:12:55 +0800

    deps: ci build

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,9 +14,9 @@ const Footer = ({ theme, count = 3 }) => {
     >
       <div>
         <span>关注本站 RSS</span>
-        <a target="_blank" href={`${window.location.origin}/rss.xml`} >
+       {typeof window !== 'undefined' &&  <a target="_blank" href={`${window.location.origin}/rss.xml`} >
           <svg aria-hidden="true" focusable="false" data-prefix="fad" data-icon="rss" class="svg-inline--fa fa-rss fa-w-14 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><g class="fa-group"><path class="fa-secondary" fill="currentColor" d="M303.74 463.21c-8.35-154.6-132.18-278.59-286.95-286.95A16 16 0 0 0 0 192.25v48.07a16 16 0 0 0 14.89 16c111.83 7.28 201.47 96.7 208.77 208.77a16 16 0 0 0 16 14.89h48.07a16 16 0 0 0 16-16.79zM16.5 32A16 16 0 0 0 0 48v48.08a16 16 0 0 0 15.45 16c191.18 7.84 344.63 161.32 352.47 352.47a16 16 0 0 0 16 15.45H432a16 16 0 0 0 16-16.5C439.6 229.68 251.46 40.45 16.5 32z"></path><path class="fa-primary" fill="currentColor" d="M0 416a64 64 0 1 1 64 64 64 64 0 0 1-64-64z"></path></g></svg>
-        </a>
+        </a>}
       </div>
       <div>{`© ${new Date().getFullYear()}, 滇ICP备19003866号`}</div>
       <div>本网站版权归本站作者Ruoduan所有</div>
